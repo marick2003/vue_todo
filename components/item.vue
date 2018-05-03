e4ae18b6f9c0ae6c414ceda594cfd8d965630ac4
marick2003 <marick2003@users.noreply.github.com>
2018-05-03 19:02:04 +0800

    fix

---
+++ components/item.vue
@@ -0,0 +1,18 @@
+<template>
+       <HelloWorld msg=""/>
+</template>
+<script>
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="">
+    
+</style>
